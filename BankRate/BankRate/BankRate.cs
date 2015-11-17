1aa299e276f02cdca5ac3c5c3f682892b8acfe54
Florin Maris <maris.flo88@gmail.com>
2015-11-17 22:34:14 +0200

    Bank Rate Calculator

---
+++ BankRate/BankRate/BankRate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankRate
+{
+  public  class BankRateCalculator
+    {
+        // Dobândă bancă descendentă
+
+        //Vrei să iei un credit de 40k euro de la bancă dobânda anuală cerută de bancă e de 7.57%.
+        //Creditul e pentru o perioadă de 20 ani contractul prevede restituirea în rate descrescătoare.
+        //Calculează ce rată trebuie plătită în luna martie din al celui de al 4-lea an?
+        //Generalizează pentru orice sumă, perioadă și dobândă.
+
+        static void Main(string[] args)
+        {
+        }
+        public static double MonthlyBankRate(int LoanValue,int CurrentMonth,int TotalPeriod,double Intrest)
+        // TotalPeriod represents the total payment period of the loan in months
+        {
+            double FixedRate = LoanValue / TotalPeriod;
+            double ToBePaid = (TotalPeriod - CurrentMonth + 1) * FixedRate;
+            double MonthlyIntrest = ToBePaid* Intrest / (12*100);
+           return FixedRate + MonthlyIntrest;
+        }
+    }
+}
---
+++ BankRate/UnitTestProject1/TestBankRate.cs
@@ -0,0 +1,29 @@
+﻿using BankRate;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankRate.Tests
+{
+    [TestClass()]
+    public class TestBankRate
+    {
+        [TestMethod()]
+        public void MonthlyBankRateTest1()
+        {
+            Assert.AreEqual(510,BankRateCalculator.MonthlyBankRate(1000,1,2,12));
+        }
+        [TestMethod()]
+        public void MonthlyBankRateTest2()
+        {
+            Assert.AreEqual(505, BankRateCalculator.MonthlyBankRate(1000, 2, 2, 12));
+        }
+        [TestMethod()]
+        public void MonthlyBankRateTest3()
+        {
+            Assert.AreEqual(1010, BankRateCalculator.MonthlyBankRate(12000, 12, 12, 12));
+        }
+      
+    }
+}
+
+
