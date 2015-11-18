b7d719839481b502b1d53f041d338dad03bca270
Florin Maris <maris.flo88@gmail.com>
2015-11-19 00:52:36 +0200

    Single day

---
+++ Goats/Goats/Goats.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Capre
+
+// În `X` zile `Y` capre mănâncă `Z` kg fân.
+// Câte kg de fân mănâncă `Q` capre în `W` zile?
+
+namespace Goats
+{
+   public class Goats
+    {
+        static void Main(string[] args)
+        {
+        }
+       public static double HayQuantity(int RefferenceGoats,int RefferenceDays,int ReffHayQuantity,
+                                           int GoatsNumber,int NumberOfDays )
+        {
+          // RefferenceQuantity represents the known quantity of hay in kg , eaten by 'RefferenceGoats' goats 
+          // in a number of 'RefferenceDays' days
+          // GoatsNumber is the number of goats for which we have to find out how many kg of hay can be eaten in 
+          // a known number of days 'NumberOfDays'
+          
+             
+            return 0;
+        }
+    }
+}
---
+++ Goats/UnitTestProject1/GoatsTests.cs
@@ -0,0 +1,17 @@
+﻿using Goats;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Goats.Tests
+{
+    [TestClass()]
+    public class GoatsTests
+    {
+        [TestMethod()]
+        public void SwitchDaysAndGoatsTest()
+        {
+            Assert.AreEqual(4,Goats.HayQuantity(1,3,4,3,1));
+        }
+    }
+}
+
