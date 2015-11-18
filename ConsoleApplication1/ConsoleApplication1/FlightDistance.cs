ba16221abae9072156537895426d45ba0c3453d7
Florin Maris <maris.flo88@gmail.com>
2015-11-18 11:18:50 +0200

    single flighr for the distance of 12km

---
+++ ConsoleApplication1/ConsoleApplication1/FlightDistance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Două trenuri pornesc în același timp unul către celălalt cu o viteză constantă de `X` km/h.
+// Când cele două trenuri sunt la o pătrime din distanța inițială, o pasăre aflată pe primul tren pornește 
+// în zbor către al 2-lea tren cu o viteză de 2X.
+// Imediat ce a ajuns la el se întoarce către primul și repetă asta până când cele două trenuri se întâlnesc.
+// Care e distanța zburată de pasăre?
+namespace ConsoleApplication1
+{
+   public class FlightDistance
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static double DistanceFlown(double DistBetweenTrains, int Birdspeed, int Trainsspeed)
+        {
+            // DistBetweenTrains represents the starting distance between the trains in kilometers
+            // Birdspeed and Trainsspeed are in km/h
+
+            return Birdspeed / Trainsspeed ;
+                }
+            }
+
+
+}
---
+++ ConsoleApplication1/UnitTestProject1/FlightDistanceTest.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApplication1;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApplication1.Tests
+{
+    [TestClass()]
+    public class FlightDistanceTest
+    {
+        [TestMethod()]
+        public void DistanceForFirstFlight()
+        {
+            Assert.AreEqual(2, FlightDistance.DistanceFlown(12, 2, 1));
+        }
+    }
+}
+
