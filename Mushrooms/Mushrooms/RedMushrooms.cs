284688e7de27d16e160f6e77ae690708a823d629
Florin Maris <maris.flo88@gmail.com>
2015-11-21 15:14:38 +0200

    First mushroom test.

---
+++ Mushrooms/Mushrooms/RedMushrooms.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mushrooms
+{
+    //## Ciuperci
+    // După ploaie au răsărit `N` ciuperci, unele roșii iar altele albe cele roșii îs de X ori mai multe.
+    // Câte ciuperci roșii sunt?
+    public class RedMushrooms
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static double RedMushroomNo(int TotalMushrooms, double MushroomRatio)
+        {
+        return 0;
+        }
+    }
+
+}
---
+++ Mushrooms/MushroomsTest/RedMusroomsTest.cs
@@ -0,0 +1,17 @@
+﻿using Mushrooms;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mushrooms.Tests
+{
+    [TestClass()]
+    public class RedMusroomsTest
+    {
+        [TestMethod()]
+        public void Test12Mushrooms()
+        {
+            Assert.AreEqual(9,RedMushrooms.RedMushroomNo(12,3));
+        }
+    }
+}
+
