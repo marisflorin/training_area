2569345a5d13dc384e437ab6396dc130eeca6240
Florin Maris <maris.flo88@gmail.com>
2015-11-21 21:27:11 +0200

    First failed test

---
+++ SquarePavement/SquarePavement/PavementBlocks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquarePavement
+{
+//   Pavaj pentru piață
+//   Primăria a decis să paveze piața mare din centru orașului cu piatră cubică.
+//   Piața e un dreptunghi de `m x n`, iar piatra cubică are dimensiunea de `a x a`.
+//   De câte bucăți de piatră e nevoie pentru a acoperi piața?
+//   Piatra cubică se vinde la bucată, deci nu pot fi cumpărate jumătăți
+
+//   Exemplu: dacă piața e de 6x6 și piatra cubică e de 4x4, rezultatul trebuie să fie 4
+   public class PavementBlocks
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static int BlockNumber(int squarelength, int squarewidth, int blocksize)
+        {
+            int BlockNumber=0;
+            return BlockNumber;
+        }
+    }
+}
---
+++ SquarePavement/SquarePavementTests/PavementTests.cs
@@ -0,0 +1,17 @@
+﻿using SquarePavement;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SquarePavement.Tests
+{
+    [TestClass()]
+    public class PavementTests
+    {
+        [TestMethod()]
+        public void TestNoDecimals()
+        {
+            Assert.AreEqual(9,PavementBlocks.BlockNumber(6,6,2));
+        }
+    }
+}
+
