484f427ad9b702598b8ff6711ddca4249c9899df
Florin Maris <maris.flo88@gmail.com>
2015-11-21 23:39:05 +0200

    First failed test

---
+++ Athlete/Athlete/RepetitionsNo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Athlete
+{
+// Sportiv
+
+//    Un sportiv a efectuat mai multe runde de pregătire.
+//    În prima rundă a efectuat o singură repetiție.
+//    În a doua rundă a efectuat două repetiții și tot așa până la runda `N` când a ajuns la `N` repetiții.
+//    După care a început să scadă din nou nr de repetiții, cu câte 1/rundă asta până ce a ajuns din nou la o singură repetiție.
+
+//    Ajută-l pe sportiv să calculeze câte repetiții a efectuat în total.
+ public  class RepetitionsNo
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static int TotalRepetitions(int RoundNumber)
+        {
+            return RoundNumber;
+        }
+    }
+}
---
+++ Athlete/AthleteTest/RepetitionsNoTests.cs
@@ -0,0 +1,17 @@
+﻿using Athlete;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Athlete.Tests
+{
+    [TestClass()]
+    public class RepetitionsNoTests
+    {
+        [TestMethod()]
+        public void TotalRepetitionsTest()
+        {
+            Assert.AreEqual(25,RepetitionsNo.TotalRepetitions(5));
+        }
+    }
+}
+
