bde6a0736f34b6c282f0b2449fb8b9bc6193171b
Florin Maris <maris.flo88@gmail.com>
2015-11-22 23:21:19 +0200

    Melon test for 6kg

---
+++ 2.1Melon/2.1Melon/MelonShare.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1Melon
+{
+    // Doi prieteni au cumpărat un pepene de X kg, și doresc să îl împartă.Singura lor dorință e ca fiecare să 
+    // primească un număr par de kg din pepene.E ok și dacă nu primesc aceeași cantitate.
+
+    // Scrie un program care răspunde cu DA dacă pepenele poate fi împărțit cum își doresc cei doi prieteni, 
+    // și cu NU în caz contrar.
+
+   public class MelonShare
+    {
+        static void Main(string[] args)
+        {
+        }
+      public static string MelonShareable(int Melonweigth)
+        {
+            // Melonweigth should be entered in kilograms
+            if (Melonweigth %2 ==0)
+            return "Yes";
+            return "No";
+
+        } 
+    }
+}
---
+++ 2.1Melon/MelonTests/MelonTest.cs
@@ -0,0 +1,17 @@
+﻿using _2._1Melon;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2._1Melon.Tests
+{
+    [TestClass()]
+    public class MelonTest
+    {
+        [TestMethod()]
+        public void MelonShareableTest()
+        {
+            Assert.AreEqual("Yes",MelonShare.MelonShareable(6));
+        }
+    }
+}
+
