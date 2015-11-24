2993223ccaa7431bab06b179ddabc1909be85164
Florin Maris <maris.flo88@gmail.com>
2015-11-25 00:04:18 +0200

    First test

---
+++ FarmersLand/FarmersLandTests/DimensionTest.cs
@@ -0,0 +1,17 @@
+﻿using FarmersLand;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FarmersLand.Tests
+{
+    [TestClass()]
+    public class DimensionTest
+    {
+        [TestMethod()]
+        public void TestFor400m()
+        {
+            Assert.AreEqual(400,Dimension.SideDimension(20,100));
+        }
+    }
+}
+
