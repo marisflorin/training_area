299b54a090092c7501c1ad00630fa2297afb3659
Florin Maris <maris.flo88@gmail.com>
2015-11-26 00:11:34 +0200

    Calculating penalty for 1 to 10 days

---
+++ 2.4RentDebt/RentDebt/TotalRent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentDebt
+{  // Ai de plătit chiria lunară.Dar cu fiecare zi de întârziere trebuie să plătești o penalitate.
+   // Dacă ai întârziat:
+    // 1-10 zile - plătești o penalitate de 2% din chirie/zi întârzire
+    // 11-30 zile - plătești o penalitate de 5% din chirie/zi întârzire    
+    // 31-40 zile - plătești o penalitate de 10% din chirie/zi întârzire
+    // Dacă ști chiria și numărul de zile de întârziere, calculează suma totală de plată.
+  public  class TotalRent
+    {
+        static void Main()
+        {}
+        public static double TotalMonthlyRent(double MonthlyRent, int DaysLate)
+        {
+            if ((DaysLate > 0) && (DaysLate <= 10)) return (MonthlyRent + (MonthlyRent * 0.02) * DaysLate);
+
+            return 0;   
+                }
+    }
+}
---
+++ 2.4RentDebt/RentDebtTests/TotalRentTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RentDebt;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RentDebtTests
+{
+    [TestClass]
+    public class TotalRentTests
+    {
+        [TestMethod]
+        public void Test10days()
+        {
+            Assert.AreEqual(118, TotalRent.TotalMonthlyRent(100, 9));
+        }
+    }
+}
