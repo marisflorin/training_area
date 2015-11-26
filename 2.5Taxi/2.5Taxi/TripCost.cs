d9fd66482aa90d31c5b3dae038be4d9c64c49c0d
Florin Maris <maris.flo88@gmail.com>
2015-11-26 20:12:30 +0200

    First Test

---
+++ 2.5Taxi/2.5Taxi/TripCost.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._5Taxi
+{
+   //  O firmă de taxi taxează în funcție de numărul de kilometrii în plus are 
+   //     tarife diferențiate pentru zi și pentru noapte.
+      
+        //   Pe timp de zi(între 8:00 și ora 21:00) tarifele sunt:
+   //   1-20km - 5 lei / km
+   //   21-60km - 4 lei / km dar se taxează dus și întors
+   //   peste 60km - 3 lei / km se taxează dus și întors
+   
+        //   Pe timp de noapte(între ora 21:00 și 8:00) tarifele sunt:
+   //   1-20km - 7 lei / km
+   //   21-60km - 5 lei / km dar se taxează dus și întors
+   //   peste 60km - 4 lei / km se taxează dus și întors
+   //   Dacă ști ce distanță trebuie parcursă și ora din zi, calculează tariful.
+  public  class TripCost
+    {
+        static void Main()
+        {}
+        public static double TotalTripCost(double Distance, double Time)
+        {
+            if ((Time > 800) && (Time < 2100))
+                return Distance * 5;
+            return 0;
+        }
+    }
+}
---
+++ 2.5Taxi/TaxiTests/TripCostsTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using _2._5Taxi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaxiTests
+{
+    [TestClass]
+    public class TripCostsTest
+    {
+        [TestMethod]
+        public void TestDayTimeUnder20km()
+        {
+            Assert.AreEqual(20, TripCost.TotalTripCost(4, 900));
+        }
+    }
+}
