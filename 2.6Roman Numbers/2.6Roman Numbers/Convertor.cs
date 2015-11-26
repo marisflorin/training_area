99cb458491a1d45e74737de3a3cd83dd92620c11
Florin Maris <maris.flo88@gmail.com>
2015-11-26 23:14:48 +0200

    Test Running for numbers up to 3

---
+++ 2.6Roman Numbers/2.6Roman Numbers/Convertor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._6Roman_Numbers
+{   // Convertește un număr dat, între 1 și 100, în cifre romane.
+    // I - 1; V - 5; X - 10; L - 50; C - 100
+    //De ex: 4 - IV; 8 - VIII; 9 - IX; 15 - XV; 30 - XXX; 39 - XXXIX 41 - XLI; 45 - XLV; 99 - XCIX
+    public class Convertor
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static string ConvertedResult(int Number)
+        {
+            string Converted = "";
+            if ((Number > 0) && (Number <= 100))
+             {
+                for (int i=1; i <= Number; i++)
+                { Converted += "I"; }
+                return Converted;
+             }
+            return "Number should be between 1 and 100";
+
+        }
+    }
+}
---
+++ 2.6Roman Numbers/RomanNumbersTest/ConvertorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using _2._6Roman_Numbers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace RomanNumbersTest
+{
+    // Convertește un număr dat, între 1 și 100, în cifre romane.
+    // I - 1; V - 5; X - 10; L - 50; C - 100
+    //De ex: 4 - IV; 8 - VIII; 9 - IX; 15 - XV; 30 - XXX; 39 - XXXIX 41 - XLI; 45 - XLV; 99 - XCIX
+
+    [TestClass]
+    public class ConvertorTests
+    {
+        [TestMethod]
+        public void TestFor3()
+        {
+            Assert.AreEqual("III", Convertor.ConvertedResult(3));
+        }
+
+    }
+}
