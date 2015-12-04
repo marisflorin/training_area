aca627c84331fba3842b283a4106a921bbb43708
Florin Maris <maris.flo88@gmail.com>
2015-12-05 01:26:25 +0200

    Second test,

---
+++ 3.1ChessBoard/3.1ChessBoard/CessBoard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._1ChessBoard
+{
+    //Pătrate pe tabla de șah
+    //Calculează câte pătrate se pot forma pe o tablă de șah de dimensiunea de 8 x 8.
+
+   public class CessBoard
+    {
+        static void Main(string[] args)
+        {}
+        public static int NumberOFSquares(int side)
+        {
+            return side;
+        }  
+    }
+}
---
+++ 3.1ChessBoard/CessBoardTests/CessBoardTests.cs
@@ -0,0 +1,21 @@
+﻿using _3._1ChessBoard;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _3._1ChessBoard.Tests
+{
+    [TestClass()]
+    public class CessBoardTests
+    {
+        [TestMethod()]
+        public void TestForOne()
+        {
+            Assert.AreEqual(1,CessBoard.NumberOFSquares(1));
+        }
+        [TestMethod()]
+        public void TestFor2()
+        {
+            Assert.AreEqual(5, CessBoard.NumberOFSquares(2));
+        }
+    }
+}
