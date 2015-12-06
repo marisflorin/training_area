53f16e62bcc33a285ed48a2165e558abf6abc895
Florin Maris <maris.flo88@gmail.com>
2015-12-07 01:38:14 +0200

    First test

---
+++ 3.3Panagram/3.3Panagram/Panagram.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._3Panagram
+{  // Panagram
+   //  Un panagram e o frază care conține toate literele din alfabet(englezesc). 
+   //  Scrie un program care determină dacă o frază e o panagram.
+  //  Exemplu: The quick brown fox jumps over the lazy dog
+    public class Panagram
+    {
+       static void Main(string[] args)
+        {
+        }
+        public static bool IsPanagram(string phrase)
+        {
+            int i = 0;
+            string panagram = "abc";
+            do
+            {
+                int j = 0;
+                while (AreMoreChars(phrase, j) && NotFoundCahr(phrase, i, panagram, j))
+                    j++;
+                if (DidCheckEntireString(phrase, j)) break;
+                i++;
+            } while (AreMoreChars(panagram,i));
+
+            return i == 3 ? true : false;
+        }
+
+        private static bool AreMoreChars(string phrase, int j)
+        {
+            return j < phrase.Length;
+        }
+
+        private static bool NotFoundCahr(string phrase, int i, string panagram, int j)
+        {
+            return panagram[i] != phrase[j];
+        }
+
+        private static bool DidCheckEntireString(string phrase, int j)
+        {
+            return j == phrase.Length;
+        }
+    }
+}
---
+++ 3.3Panagram/PanagramTests/PanagramTests.cs
@@ -0,0 +1,17 @@
+﻿using _3._3Panagram;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _3._3Panagram.Tests
+{
+    [TestClass()]
+    public class PanagramTests
+    {
+        [TestMethod()]
+        public void IsPanagramTest()
+        {
+            Assert.AreEqual(true,Panagram.IsPanagram("abc"));
+        }
+    }
+}
+
