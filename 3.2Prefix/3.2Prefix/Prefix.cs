885e4a206d77b00f4bd294d5bb1d94b3250b9ee7
Florin Maris <maris.flo88@gmail.com>
2015-12-06 15:00:42 +0200

    Failing test

---
+++ 3.2Prefix/3.2Prefix/Prefix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2Prefix
+{
+   public class Prefix
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static string FindPrefix(string firststring, string secoundstring)
+        {
+            string prefix = "";
+            return prefix;
+        }
+    }
+}
---
+++ 3.2Prefix/Prefix/PrefixTests.cs
@@ -0,0 +1,17 @@
+﻿using _3._2Prefix;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _3._2Prefix.Tests
+{
+    [TestClass()]
+    public class PrefixTests
+    {
+        [TestMethod()]
+        public void FindPrefixTest()
+        {
+            Assert.AreEqual("aa",Prefix.FindPrefix("aab","aac"));
+        }
+    }
+}
+
