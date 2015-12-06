f512ab719264d8fee24e29cb9ab903f839da30be
Florin Maris <maris.flo88@gmail.com>
2015-12-06 16:27:22 +0200

    First test

--- 3.2Prefix/3.2Prefix/Prefix.cs
+++ 3.2Prefix/3.2Prefix/Prefix.cs
@@ -11,10 +11,25 @@ namespace _3._2Prefix
         static void Main(string[] args)
         {
         }
-        public static string FindPrefix(string firststring, string secoundstring)
+        public static string FindPrefix(string string1, string string2)
         {
             string prefix = "";
+            int steps = GetShortStringLength(string1, string2);
+            int i = 0;
+            while (i <= GetShortStringLength(string1,string2) && string1[i]==string2[i]) 
+            {
+                prefix += string1[i];
+                i++;
+            }
+
             return prefix;
         }
+
+        private static int GetShortStringLength(string firststring, string secoundstring)
+        {
+            int shortestlength = firststring.Length >= secoundstring.Length ? firststring.Length : secoundstring.Length;
+            return shortestlength;
+        }
+    
     }
 }
