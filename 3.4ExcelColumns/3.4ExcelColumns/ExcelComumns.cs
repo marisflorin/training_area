7e7c79a6901f9da922df9ead49f9ccaff9786947
Florin Maris <maris.flo88@gmail.com>
2015-12-08 21:42:00 +0200

    Test for one letter column names

---
+++ 3.4ExcelColumns/3.4ExcelColumns/ExcelComumns.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._4ExcelColumns
+{
+    public class ExcelComumns
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static string ColumnName(int columnindex)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string columnname="";
+            columnindex -= 1;
+            columnname += alphabet[columnindex];
+            return columnname;
+        }
+    }
+}
---
+++ 3.4ExcelColumns/ExcelColumnsTest/ColumnsTest.cs
@@ -0,0 +1,17 @@
+﻿using _3._4ExcelColumns;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _3._4ExcelColumns.Tests
+{
+    [TestClass()]
+    public class ColumnsTest
+    {
+        [TestMethod()]
+        public void ColumnNameTest()
+        {
+            Assert.AreEqual("D",ExcelComumns.ColumnName(4));
+        }
+    }
+}
+
