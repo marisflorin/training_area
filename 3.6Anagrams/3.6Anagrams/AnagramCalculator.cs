b8d0f2704cd46505f2d9c55c192455660b6fb9cd
Florin Maris <maris.flo88@gmail.com>
2015-12-11 00:44:42 +0200

    anagrams first test

---
+++ 3.6Anagrams/3.6Anagrams/AnagramCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._6Anagrams
+{
+  public  class AnagramCalculator
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static int CalculateAnagramNum(string anagram)
+
+        {
+            return Factorial(anagram.Length)-1;
+        }    
+            private static int Factorial(int totalletters)
+        {
+            int f = 1;
+            for (int i = 1; i <= totalletters; i++)
+                f *= i;
+            return f;
+        }
+        }
+    }
+
---
+++ 3.6Anagrams/AnagramCalculatorTests/AnagramCalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using _3._6Anagrams;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _3._6Anagrams.Tests
+{
+    [TestClass()]
+    public class AnagramCalculatorTests
+    {
+        [TestMethod()]
+        public void CalculateAnagramNumTest()
+        {
+            Assert.AreEqual(1,AnagramCalculator.CalculateAnagramNum("ab"));
+        }
+    }
+}
