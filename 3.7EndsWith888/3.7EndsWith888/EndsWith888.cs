be6de97089369de73fc1cb97cbbbd652464afd9b
Florin Maris <maris.flo88@gmail.com>
2015-12-17 00:07:39 +0200

    Final version, working for very big numbers

--- 3.7EndsWith888/3.7EndsWith888/EndsWith888.cs
+++ 3.7EndsWith888/3.7EndsWith888/EndsWith888.cs
@@ -17,18 +17,82 @@ namespace _3._7EndsWith888
         {
         }
         public static double GetNumber(int rootNumber, int endingDigits)
-            {
+        {
 
-            int rootCount=0;
+            int rootCount = 0;
             double cubeRoot = 0;
+            double[] solutionArray = new double[1];
             do
             {
                 cubeRoot++;
-                if (cubeRoot*cubeRoot*cubeRoot % 1000 == endingDigits) rootCount++;               
-            } while (rootCount < rootNumber);
+                if (IsValidRoot(endingDigits, cubeRoot))
+                {
+                    AddSolutionToArray(ref rootCount, cubeRoot, ref solutionArray);
+                }
 
-            return cubeRoot;
-             
-            }    
+            } while (rootCount < rootNumber && IsNotBiggerDigitNumber(endingDigits, cubeRoot));
+
+            if (rootCount == rootNumber) return solutionArray[rootCount - 1];
+            int root = GetFirstDigits(rootNumber, ref rootCount, solutionArray);
+            int solutionIndex = GetIndex(rootCount, solutionArray);
+            return BuildNumber(endingDigits, solutionArray, root, solutionIndex);
+
+        }
+
+        private static double BuildNumber(int endingDigits, double[] solutionArray, int root, int solutionIndex)
+        {
+            return root * (Math.Pow(10, GetNumberOfDigits(endingDigits))) + solutionArray[solutionIndex];
+        }
+
+        private static int GetIndex(int rootCount, double[] solutionArray)
+        {
+            return rootCount % solutionArray.Length != 0 ? rootCount % solutionArray.Length - 1 : 3;
+        }
+
+        private static int GetFirstDigits(int rootNumber, ref int rootCount, double[] solutionArray)
+        {
+            int root = 0;
+            while (rootCount < rootNumber)
+            {
+                root++;
+                foreach (int element in solutionArray)
+                {
+                    rootCount++;
+                    if (rootCount == rootNumber) break;
+                }
+            }
+
+            return root;
+        }
+
+        private static void AddSolutionToArray(ref int rootCount, double cubeRoot, ref double[] solutionArray)
+        {
+            int i = 0;
+            double[] temp = new double[rootCount + 1];
+            foreach (double element in solutionArray)
+            {
+                temp[i] = element;
+                i++;
+            }
+            temp[rootCount] = cubeRoot;
+            solutionArray = temp;
+            rootCount++;
+        }
+
+        private static bool IsNotBiggerDigitNumber(int endingDigits, double cubeRoot)
+        {
+            return GetNumberOfDigits(cubeRoot) <= GetNumberOfDigits(endingDigits);
+        }
+
+        private static bool IsValidRoot(int endingDigits, double cubeRoot)
+        {
+            int power = GetNumberOfDigits(endingDigits);
+            return Math.Pow(cubeRoot,3) % Math.Pow(10, power) == endingDigits;
+        }
+
+        private static int GetNumberOfDigits(double endingDigits)
+        {
+            return endingDigits.ToString().Length;
+        }
     }
 }
--- 3.7EndsWith888/EndsWith888/EndsWith888Tests.cs
+++ 3.7EndsWith888/EndsWith888/EndsWith888Tests.cs
@@ -8,9 +8,24 @@ namespace _3._7EndsWith888.Tests
     public class EndsWith888Tests
     {
         [TestMethod()]
-        public void GetNumberTest()
+        public void GetFirstNumberTest()
         {
             Assert.AreEqual(192,EndsWith888.GetNumber(1,888));
+        }
+        [TestMethod()]
+        public void GetNumberForTwoDigitsTest()
+        {
+            Assert.AreEqual(92, EndsWith888.GetNumber(2, 88));
+        }
+        [TestMethod()]
+        public void GetLastThreeDigitNumberTest()
+        {
+            Assert.AreEqual(942, EndsWith888.GetNumber(4, 888));
+        }
+        [TestMethod()]
+        public void GetFourDigitNumberTest()
+        {
+            Assert.AreEqual(2192, EndsWith888.GetNumber(9, 888));
         }
     }
 }
