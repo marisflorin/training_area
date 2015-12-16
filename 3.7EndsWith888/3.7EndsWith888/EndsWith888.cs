7ac1dcfda926c0315f87d948122d4303b02a2368
Florin Maris <maris.flo88@gmail.com>
2015-12-16 19:40:41 +0200

    Test for 192

---
+++ 3.7EndsWith888/3.7EndsWith888/EndsWith888.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._7EndsWith888
+{
+    // Cub
+    // Un om de știință vrea să știe toate numerele al căror cub se termină cu 888. 
+    // Ajută-l prin a-i face un program care pentru numărul k, 
+    // întoarce al k-lea număr al cărui cub e 888.
+    //  Exemplu: dacă k e 1, rezultatul e 192
+    public class EndsWith888
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static double GetNumber(int rootNumber, int endingDigits)
+            {
+
+            int rootCount=0;
+            double cubeRoot = 0;
+            do
+            {
+                cubeRoot++;
+                if (cubeRoot*cubeRoot*cubeRoot % 1000 == endingDigits) rootCount++;               
+            } while (rootCount < rootNumber);
+
+            return cubeRoot;
+             
+            }    
+    }
+}
---
+++ 3.7EndsWith888/EndsWith888/EndsWith888Tests.cs
@@ -0,0 +1,16 @@
+﻿using _3._7EndsWith888;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _3._7EndsWith888.Tests
+{
+    [TestClass()]
+    public class EndsWith888Tests
+    {
+        [TestMethod()]
+        public void GetNumberTest()
+        {
+            Assert.AreEqual(192,EndsWith888.GetNumber(1,888));
+        }
+    }
+}
