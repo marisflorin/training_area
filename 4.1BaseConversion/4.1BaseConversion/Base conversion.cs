610c3a5f3979772986ea4a077ebbf257015739be
Florin Maris <maris.flo88@gmail.com>
2016-01-07 16:50:11 +0200

    First tests

---
+++ 4.1BaseConversion/4.1BaseConversion/Base conversion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1BaseConversion
+{ /* Operații în baza doi
+  Ai un număr zecimal transformă-l în baza doi, reprezentat ca și un array de byte (byte[]).
+  Pentru un număr transformat implementează operațiile:
+  NOT, AND, NOT, OR, XOR
+  RightHandShift și LeftHandShift(shiftare de biți la dreapta și la stânga)
+  LessThan
+  Adunare și scădere
+  Înmulțire și împărțire
+  Folosește-te de LessThan pentru a implementa și alți operatori de comparare(GraterThan, Equal, NotEqual)
+  Doar pentru numere pozitive.
+  Poți generaliza transformarea și operațiile de la 3-6 pentru o bază aleatoare (baza fiind între 2 și 255)?
+  */
+   public class BaseConversion
+    {
+        static void Main(string[] args)
+        {
+        }
+    public static byte[] ConvertToBit(int number)
+        {
+            byte[] converted = new byte[8];
+            int i = 1;
+ 
+            while (number/2!=0 || number%2!=0)
+            {
+                int position = converted.Length - i;
+                if (number % 2 == 1) converted[position] = 1;
+                else converted[position] = 0;
+                number = number / 2;
+                i++;
+                
+                
+            }
+
+
+            return converted;
+        }
+      
+    }
+}
+
---
+++ 4.1BaseConversion/BaseConversionTests/BaseConversionTests.cs
@@ -0,0 +1,56 @@
+﻿using _4._1BaseConversion;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+
+namespace _4._1BaseConversion.Tests
+{
+    [TestClass()]
+    public class BaseConversionTests
+    {
+        [TestMethod()]
+        public void ConvertToBitTestfor156()
+        {
+            List<byte> first = new List<byte>();
+            for (int i = 0; i < BaseConversion.ConvertToBit(156).Length; i++)
+            {
+                first.Add(BaseConversion.ConvertToBit(156)[i]);
+            }
+
+            List<byte> second = new List<byte>();
+            second.Add(1);
+            second.Add(0);
+            second.Add(0);
+            second.Add(1);
+            second.Add(1);
+            second.Add(1);
+            second.Add(0);
+            second.Add(0);
+
+            CollectionAssert.AreEqual(first, second);
+          }
+        [TestMethod()]
+        public void ConvertToBitTestfor256()
+        {
+            List<byte> first = new List<byte>();
+            for (int i = 0; i < BaseConversion.ConvertToBit(256).Length; i++)
+            {
+                first.Add(BaseConversion.ConvertToBit(256)[i]);
+            }
+
+            List<byte> second = new List<byte>();
+            second.Add(1);
+            second.Add(0);
+            second.Add(0);
+            second.Add(0);
+            second.Add(0);
+            second.Add(0);
+            second.Add(0);
+            second.Add(0);
+            second.Add(0);
+
+            CollectionAssert.AreEqual(first, second);
+        }
+    }
+}
