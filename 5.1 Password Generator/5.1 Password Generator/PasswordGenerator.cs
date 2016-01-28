2e9ad6265c5fef93a22e0510c934b5be48274a21
Florin Maris <maris.flo88@gmail.com>
2016-01-28 20:57:24 +0200

    small letter password

---
+++ 5.1 Password Generator/5.1 Password Generator/PasswordGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1_Password_Generator
+{
+    /*
+    Scrie o funcție care generează o parolă de până la X caractere. Funcția oferă ca și opțiuni:
+
+    1.litere mici
+    2.litere mari și numărul lor
+    3.cifre și numărul lor
+    4.simboluri și numărul lor
+    5.să nu includă caracterele similare: l, 1, L, o, 0, O
+    6.să nu includă caractere abigue: {}[]()/\'"~,;.<>
+    */
+ public class PasswordGenerator
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void PasswordBuilder(byte passLength, ref string password)
+        {
+            int pos = 0;
+            char r;
+            password = "";
+            Random rnd = new Random();
+            for (byte i=0;i<passLength;i++)
+            {
+                pos = rnd.Next(0, 26);
+                r = (char)('a' + pos);
+                password += r;
+            }   
+        }
+
+    }
+}
---
+++ 5.1 Password Generator/5.1 PasswordTests/PasswordGeneratorTests.cs
@@ -0,0 +1,36 @@
+﻿using _5._1_Password_Generator;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _5._1_PasswordTests
+{
+    [TestClass]
+    public class PasswordGeneratorTests
+    {
+        [TestMethod]
+        public void PasswordLength()
+        {
+            string password="";
+            PasswordGenerator.PasswordBuilder(100, ref password);
+            Assert.AreEqual(100, password.Length);
+        }
+        [TestMethod]
+        public void TestIfSmallLetter()
+        {
+            string password = "";
+            string smallLetters = "abcdefghijklmnopqrstuvwxyz";
+            PasswordGenerator.PasswordBuilder(100, ref password);
+            bool onlySmallLetters=false;
+            for (int i = 0; i < password.Length; i++)
+            {
+                onlySmallLetters = false;
+                for (int j = 0; j < smallLetters.Length; j++)
+                {
+                    if (password[i] == smallLetters[j]) onlySmallLetters = true;
+                }
+                if (onlySmallLetters == false) break;
+            }
+            Assert.AreEqual(true, onlySmallLetters);
+        }
+    }
+}
