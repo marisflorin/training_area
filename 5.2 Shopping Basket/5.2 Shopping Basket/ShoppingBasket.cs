6bfc4e307d88107f23147374477d62f54026b741
Florin Maris <maris.flo88@gmail.com>
2016-02-14 20:02:51 +0200

    Calculating total price

---
+++ 5.2 Shopping Basket/5.2 Shopping Basket/ShoppingBasket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace _5._2_Shopping_Basket
+{
+    /*Cumpărături
+Ai un coș plin de produse. Dacă cunoști prețul fiecărui produs în parte:
+
+1.Calculează care e totalul de plată pentru un coșul cu cumpărături
+2.Găsește și care e cel mai ieftin produs din coș
+3.Elimină cel mai scump produs din coș
+4.Adaugă un nou produs în coș
+5.Calculează prețul mediu */
+    public struct Item
+    {
+        public string name;
+        public int price;
+    }
+
+   public class ShoppingBasket
+    {
+                static void Main(string[] args)
+        {
+        }
+               public Item[] shoppingBasket = new Item[10];      
+       public static int TotalPrice(Item[] shoppingBasket)
+        {
+            int totalPrice = 0;
+            for (int i = 0; i < shoppingBasket.Length; i++)
+                totalPrice += shoppingBasket[i].price;
+            return totalPrice;
+                
+                
+        }
+    }
+}
---
+++ 5.2 Shopping Basket/5.2 ShoppingBasketTests/ShoppingBasketTests.cs
@@ -0,0 +1,27 @@
+﻿using _5._2_Shopping_Basket;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace _5._2_ShoppingBasketTests
+{
+    
+    [TestClass]
+    public class ShoppingBasketTests
+    {
+       
+        [TestMethod]
+        public void TestTotalSum()
+        {
+            Item[] shoppingBasket = new Item[] { new Item() { name ="Milk", price = 6 },
+                                                 new Item() { name ="Eggs", price=12},
+                                                 new Item() { name ="Bread", price=2}
+                                               };
+
+            Assert.AreEqual(20, ShoppingBasket.TotalPrice(shoppingBasket));
+           
+
+        }
+
+    }
+}
