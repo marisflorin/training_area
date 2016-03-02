6187f048afbce2fabe7f233910e5e7c43455ef9a
Maris Florin <maris.flo88@gmail.com>
2016-03-02 22:53:02 +0200

    Test for generating coordinates

---
+++ 5.3 Intersection/5.3 Intersection/Intersection.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3_Intersection
+{
+   /* Ți se dă o listă de tronsoane verticale și orizontale consecutive de dimensiuni fixe.
+
+Scrie o funcție care verifică dacă tronsoanele se intersectează. Dacă se intersectează întoarce
+primul punct de intersecție.
+
+Notă: având în vedere că tronsoanele îs de dimensiuni fixe, alege cel mai simplu mod de a le reprezenta,
+de ex: ca și o succesiune de direcții stânga, dreapta, sus, jos. 
+*/
+public enum Directions
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+
+    }
+public struct Point
+    {
+        public int x,y;
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+public struct Result
+    {
+        public bool isIntersection;
+        public Point firstIntersection;    
+    }   
+    
+    public class Intersection
+    {
+        public Directions[] route;
+        public Point[] routeCoordinates;
+       
+        static void Main(string[] args)
+        {
+        }
+        /*public static Result CheckIntersection(Directions[] list, Point startingPoint)
+        {
+          
+
+        }*/
+        public static void GenerateCoordinates(ref Point[] routeCoordinates, Directions[] route,Point startingPoint  )
+        {
+            routeCoordinates = new Point[route.Length+1];
+            routeCoordinates[0].x = startingPoint.x;
+            routeCoordinates[0].y = startingPoint.y;
+            for (int i = 0; i < route.Length; i++)
+            {
+
+                routeCoordinates[i+1].x = routeCoordinates[i].x;
+                routeCoordinates[i+1].y = routeCoordinates[i].y;
+                switch (route[i])
+                {
+                    case Directions.Up:
+                        ++routeCoordinates[i+1].y;
+                        break;
+                    case Directions.Down:
+                        --routeCoordinates[i+1].y;
+                        break;
+                    case Directions.Right:
+                        ++routeCoordinates[i+1].x;
+                        break;
+                    case Directions.Left:
+                        --routeCoordinates[i+1].x;
+                        break;
+                }
+            }          
+
+        }
+
+    }
+}
---
+++ 5.3 Intersection/IntersectionTests/IntersectionTests.cs
@@ -0,0 +1,23 @@
+﻿using _5._3_Intersection;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _5._3_Intersection.Tests
+{
+    [TestClass()]
+    public class IntersectionTests
+    {
+        [TestMethod()]
+        public void GenerateCoordinatesTest()
+        {
+            Point[] routeCoordinates=new Point[1];
+            Point startingPoint = new Point(0, 0);
+            Point[] testCoordinates = { new Point(0,0), new Point(0, 1), new Point(0, 2), new Point(1, 2), new Point(1, 1), new Point(0, 1), new Point(0, 0), };
+            Directions[] route = { Directions.Up, Directions.Up, Directions.Right,Directions.Down,Directions.Left,Directions.Down };
+            Intersection.GenerateCoordinates(ref routeCoordinates, route, startingPoint);
+            CollectionAssert.AreEqual(routeCoordinates,testCoordinates);
+
+        }
+    }
+}
+
