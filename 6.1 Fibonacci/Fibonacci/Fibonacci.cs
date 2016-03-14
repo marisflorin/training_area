6dbffb9d149b56c6b5198dde434f1064afdf036d
Maris Florin <maris.flo88@gmail.com>
2016-03-14 22:33:01 +0200

    test for 8

---
+++ 6.1 Fibonacci/Fibonacci/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+     /* 
+     Calculează recursiv al k-lea număr din șirul Fibonacci.
+     Șirul Fibonacii este o serie de numere: 0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+     */
+    public class Fibonacci
+    {
+        public static void Main(string[] args)
+        {
+        }
+  
+        public static int CalculateFibonacci(int x)
+        {
+
+            if (x < 4)
+                return 1;
+            return CalculateFibonacci(x - 1) + CalculateFibonacci(x - 2);
+        }
+        
+    }
+}
---
+++ 6.1 Fibonacci/FibonacciTests/FibonacciTests.cs
@@ -0,0 +1,16 @@
+﻿using Fibonacci;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fibonacci.Tests
+{
+    [TestClass()]
+    public class FibonacciTests
+    {
+        [TestMethod()]
+        public void TestForEight()
+        {
+            Assert.AreEqual(13,Fibonacci.CalculateFibonacci(8));
+        }
+    }
+}
