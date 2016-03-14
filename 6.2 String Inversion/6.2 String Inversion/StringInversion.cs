212f0c4af33c510862f269d0fb829b64913c2b1e
Maris Florin <maris.flo88@gmail.com>
2016-03-14 23:47:03 +0200

    first test

---
+++ 6.2 String Inversion/6.2 String Inversion/StringInversion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2_String_Inversion
+{
+   public class StringInversion
+    {
+        static void Main(string[] args)
+        {
+        }
+    public static string Invert(string s)
+        {
+            if (s.Length == 1) return s;
+            return s[s.Length - 1] + Invert(s.Remove(s.Length - 1));
+        }
+    }
+}
---
+++ 6.2 String Inversion/String InversionTests/StringInversionTests.cs
@@ -0,0 +1,21 @@
+﻿using _6._2_String_Inversion;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _6._2_String_Inversion.Tests
+{/* 
+    Inversare
+    Inversează un string folosind recursivitatea.
+    */
+    [TestClass()]
+    public class StringInversionTests
+    {
+        [TestMethod()]
+        public void OneWordTest()
+        {
+            Assert.AreEqual("esuom",StringInversion.Invert("mouse"));
+        }
+    }
+}
+
+
