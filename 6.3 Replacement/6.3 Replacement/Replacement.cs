7ceafeb558d6b5ac1ae31f9e64b500a430b9acf6
Maris Florin <maris.flo88@gmail.com>
2016-03-15 22:24:18 +0200

    First test

---
+++ 6.3 Replacement/6.3 Replacement/Replacement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._3_Replacement
+{ /*Înlocuire
+Înlocuiește un caracter dintr-un string cu un alt string.
+  */
+   public class Replacement
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static string Replace(string initial, string replacement, int index)
+        {
+            if (initial.Length == index) return initial.Remove(initial.Length - 1) + replacement;
+            return Replace(initial.Remove(initial.Length - 1), replacement, index) 
+                + initial[initial.Length - 1];
+
+        }
+    }
+}
---
+++ 6.3 Replacement/ReplacementTests/ReplacementTests.cs
@@ -0,0 +1,16 @@
+﻿using _6._3_Replacement;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _6._3_Replacement.Tests
+{
+    [TestClass()]
+    public class ReplacementTests
+    {
+        [TestMethod()]
+        public void ReplaceTest()
+        {
+            Assert.AreEqual("TheClearWater",Replacement.Replace("The Water","Clear",4));
+        }
+    }
+}
