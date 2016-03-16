4509c2ea55d56ae26f63c2fdfa1b2534cf23fb5e
Maris Florin <maris.flo88@gmail.com>
2016-03-16 23:08:34 +0200

    Test only for adition

---
+++ 6.4 PrefixCalculator/PrefixCalculator/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrefixCalculator
+{
+    public class Calculator
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static double PrefixCalculator(string[] input, int index = 0)
+        {
+            if (input.Length == 1) return Convert.ToInt32(input[0]);
+            if (input[index] == "+" || input[index] == "-") return PrefixCalculator(input, ++index);
+            double result = Convert.ToInt32(input[index]) + Convert.ToInt32(input[index + 1]);
+            input[index + 1] = Convert.ToString(result);
+            ShiftToFeft(ref input, index - 1);    
+            return PrefixCalculator(input,0) ;
+        }
+        public static void ShiftToFeft(ref string[] array,int index,int positions=2)
+        {
+            for (int i=index; i<array.Length-positions; i++)
+                array[i] = array[i + 2];
+            Array.Resize(ref array, array.Length-2);
+        }
+    }
+}
---
+++ 6.4 PrefixCalculator/PrefixCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using PrefixCalculator;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrefixCalculator.Tests
+{/*
+    Calculator
+Scrie un program care imită un calculator de buzunar în formă prefixată.
+Operațiile posibile sunt:
+adunare/scădere
+înmulțire/împărțire
+suportă numere reale
+Notă: În forma prefixată operatorii apar înainte operanzilor.
+Exemple pentru format prefixată:
+* 3 4 e echivalent cu 3 * 4
+* + 1 1 2, e echivalent cu (1 + 1) * 2
++ / * + 56 45 46 3 - 1 0.25 e echivalent cu ((56 + 45) * 46) / 3 + (1 - 0.25)
+*/
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void PrefixCalculatorTest()
+        {
+            string[] input = { "+", "3", "4" };
+            Assert.AreEqual(7,Calculator.PrefixCalculator(input));
+        }
+    }
+}
