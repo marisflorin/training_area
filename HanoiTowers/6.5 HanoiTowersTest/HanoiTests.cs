d83c5ffd77a2c1ac9ffb6f7da0ed75fd55459530
Maris Florin <maris.flo88@gmail.com>
2016-04-25 01:05:18 +0300

    Test for one Disk running

--- HanoiTowers/6.5 HanoiTowersTest/HanoiTests.cs
+++ HanoiTowers/6.5 HanoiTowersTest/HanoiTests.cs
@@ -16,19 +16,66 @@ Turnurile din Hanoi
     public class HanoiTests
     {
         [TestMethod()]
+        public void TestForOne()
+        {
+            string[] firstTower = new string[2];
+            string[] auxTower = new string[2];
+            string[] secondTower = new string[2];
+            firstTower[0] = "1"; auxTower[0] = ""; secondTower[0] = "";
+            firstTower[1] = ""; auxTower[1] = ""; secondTower[1] = "1";            
+            ulong level = 0;
+            string[] generatedFirstTower= new string[1],generatedSecondTower = new string[1], generatedAuxTower = new string[1];
+            Hanoi.InitializeArrays(1, ref generatedFirstTower, ref generatedAuxTower, ref generatedSecondTower);
+            Hanoi.GenerateMoves(1, ref generatedFirstTower, ref generatedAuxTower, ref generatedSecondTower, ref level);
+            CollectionAssert.AreEqual(firstTower, generatedFirstTower);
+            CollectionAssert.AreEqual(auxTower, generatedAuxTower);
+            CollectionAssert.AreEqual(secondTower, generatedSecondTower);
+        }
+        [TestMethod()]
         public void TestForTwo()
         {
-            Assert.AreEqual(3.0,Hanoi.MovesNumber(2));
+            string[] firstTower = new string[4];
+            string[] auxTower = new string[4];
+            string[] secondTower = new string[4];
+            firstTower[0] = "2 1"; auxTower[0] = ""; secondTower[0] = "";
+            firstTower[1] = "2"; auxTower[1] = "1"; secondTower[1] = "";
+            firstTower[2] = ""; auxTower[2] = "1"; secondTower[2] = "2";
+            firstTower[3] = ""; auxTower[3] = ""; secondTower[3] = "2 1";            
+            ulong level = 0;
+            string[] generatedFirstTower = new string[1], generatedSecondTower = new string[1], generatedAuxTower = new string[1];
+            Hanoi.InitializeArrays(1, ref generatedFirstTower, ref generatedAuxTower, ref generatedSecondTower);
+            Hanoi.GenerateMoves(1, ref generatedFirstTower, ref generatedAuxTower, ref generatedSecondTower, ref level);
+            CollectionAssert.AreEqual(firstTower, generatedFirstTower);
+            CollectionAssert.AreEqual(auxTower, generatedAuxTower);
+            CollectionAssert.AreEqual(secondTower, generatedSecondTower);
         }
         [TestMethod()]
         public void TestForThree()
         {
-            Assert.AreEqual(7.0, Hanoi.MovesNumber(3));
+            string[] firstTower= new string[8];
+            string[] auxTower = new string[8];
+            string[] secondTower = new string[8];
+            firstTower[0] = "3 2 1"; auxTower[0] = ""; secondTower[0] = "";
+            firstTower[1] = "3 2"; auxTower[1] = ""; secondTower[1] = "1";
+            firstTower[2] = "3";auxTower[2] = "2"; secondTower[2] = "1";
+            firstTower[3] = "3";auxTower[3] = "2 1";secondTower[3] = "";
+            firstTower[4] = ""; auxTower[4] = "2 1"; secondTower[4] = "3";
+            firstTower[5] = "1";auxTower[5] = "2";secondTower[5] = "3";
+            firstTower[6] = "1";auxTower[6] = "";secondTower[6] = "3 2";
+            firstTower[7] = ""; auxTower[7] = "";secondTower[7] = "3 2 1";
+            ulong level = 0;
+            string[] generatedFirstTower = new string[1], generatedSecondTower = new string[1], generatedAuxTower = new string[1];
+            Hanoi.InitializeArrays(1, ref generatedFirstTower, ref generatedAuxTower, ref generatedSecondTower);
+            Hanoi.GenerateMoves(1, ref generatedFirstTower, ref generatedAuxTower, ref generatedSecondTower, ref level);
+            CollectionAssert.AreEqual(firstTower, generatedFirstTower);
+            CollectionAssert.AreEqual(auxTower, generatedAuxTower);
+            CollectionAssert.AreEqual(secondTower, generatedSecondTower);
         }
-        [TestMethod()]
+
+       /* [TestMethod()]
         public void TestFor64()
         {
             Assert.AreEqual(18446744073709551615, Hanoi.MovesNumber(64));
-        }
+        } */
     }
 }
--- HanoiTowers/HanoiTowers/Hanoi.cs
+++ HanoiTowers/HanoiTowers/Hanoi.cs
@@ -5,16 +5,48 @@ using System.Text;
 using System.Threading.Tasks;
 
  namespace HanoiTowers
-{
-    public class Hanoi
+{   
+       public class Hanoi
     {
         public static void Main(string[] args)
         {
         }
-        public static ulong MovesNumber(int numberOfDisks)
+        public static void InitializeArrays(int numberOfDisks,ref string[] first,ref string[] aux,ref string[] second)
         {
-            if (numberOfDisks == 1) return 1;
-            return 2 * MovesNumber(numberOfDisks - 1) + 1;
+            ulong numberOfMoves = (ulong)(2 ^ numberOfDisks - 1);
+            first = new string[numberOfMoves];
+            aux = new string[numberOfMoves];
+            second = new string[numberOfMoves];
+            for (ulong i=0; i<numberOfMoves;i++ )
+            {
+                if (i < (ulong)numberOfDisks)
+                    first[i] = Convert.ToString(numberOfDisks - (int)i);
+                else first[i] = "";
+                aux[i] = "";
+                second[i] = "";
+            }
+
+        }
+
+        public static void GenerateMoves(int numberOfDisks,ref string[] firstTower,ref string[] auxTower,ref string[] secondTower, ref ulong level)
+        {
+
+            if (numberOfDisks == 1)
+            {
+                firstTower[level + 1] = "";
+                if (secondTower[level]=="") secondTower[level + 1] =firstTower[level];
+                else secondTower[level + 1] = secondTower[level] + " " + firstTower[level];
+                level++;
+            }
+            else
+            {
+                GenerateMoves(--numberOfDisks, ref firstTower, ref secondTower, ref auxTower, ref level);
+                firstTower[level + 1] = "";
+                /* cutt one off */
+                secondTower[level + 1] = secondTower[level] + " " + firstTower[level];
+                level++;
+                GenerateMoves(--numberOfDisks, ref auxTower, ref firstTower, ref secondTower, ref level);
+            }
         }
     }
 }
