21ebd00207e02d6ad50bf14e0a2b06439b62d6fc
Maris Florin <maris.flo88@gmail.com>
2016-05-05 01:34:38 +0300

    First test running

---
+++ 6.6 Pascal Triangle/6.6 Pascal Triangle/Pascal Triangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._6_Pascal_Triangle
+{
+   public class Pascal
+    {
+        
+        public static void GenerateElement(int level,ref int[][] triangle,int index)
+        {
+            if (level-1 ==0)
+            {
+                triangle[0] = new int[] { 1 };
+                return;
+            }
+            if (index == 0)
+                {
+                triangle[level - 1] = new int[level];
+                triangle[level-1][index] = 1;
+                GenerateElement(level, ref triangle, ++index);
+                }
+            if (index==level-1)
+                {
+                triangle[level-1][index] = 1;
+                GenerateElement(level-1, ref triangle, 0);
+                }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 6.6 Pascal Triangle/PascalTriangleTests/PascalTriangleTests.cs
@@ -0,0 +1,24 @@
+﻿using _6._6_Pascal_Triangle;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _6._6_Pascal_Triangle.Tests
+{
+  
+    [TestClass()]
+    public class PascalTriangleTests
+    {
+        [TestMethod()]
+        public void GenerateTriangleTest()
+        {
+            int[][] testTriangle = {
+                new int[] { 1 },
+                new int[] { 1, 1 }};
+            int[][] triangle = { new int[] { 1 }, new int[] { } };
+            int index = 0;
+            Pascal.GenerateElement(2,ref triangle,index);
+            CollectionAssert.AreEqual(triangle[0],testTriangle[0]);
+            CollectionAssert.AreEqual(triangle[1],testTriangle[1]);
+        }
+    }
+}
