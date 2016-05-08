7e134560890087963ac33182fada43397afcf25d
Maris Florin <maris.flo88@gmail.com>
2016-05-08 23:56:21 +0300

    First and final test

---
+++ 7.1. Loto/7.1 LotoTests/LotoTests.cs
@@ -0,0 +1,37 @@
+﻿using _7._1.Loto;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _7._1.Loto.Tests
+{
+    /*Extragere loto
+Trebuie ca tot timpul numerele extrase la loto să fie afișate la public în ordine crescătoare
+și nu în ordinea în care au fost extrase.
+Scrie o funcție care face asta.
+*/
+    [TestClass()]
+    public class LotoTests
+    {
+        [TestMethod]
+        public void TestLotoNumbers()
+        {
+            int[] extractedSeq = { 23, 12, 2, 45, 8, 10 };
+            int[][] printResult =  {
+               new int[] {23},
+               new int[] {12,23 },
+               new int[] {2,12,23 },
+               new int[] {2,12,23,45 },
+               new int[] {2,8,12,23,45},
+               new int[] {2,8,10,12,23,45 }
+                };
+            int[][] printSequence = new int[6][];
+            Loto.PrintOut(extractedSeq,ref printSequence);
+            CollectionAssert.AreEqual(printResult[0], printSequence[0]);
+            CollectionAssert.AreEqual(printResult[1], printSequence[1]);
+            CollectionAssert.AreEqual(printResult[2], printSequence[2]);
+            CollectionAssert.AreEqual(printResult[3], printSequence[3]);
+            CollectionAssert.AreEqual(printResult[4], printSequence[4]);
+            CollectionAssert.AreEqual(printResult[5], printSequence[5]);            
+        }
+    }
+}
---
+++ 7.1. Loto/7.1. Loto/Loto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1.Loto
+{
+    public class Loto
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void PrintOut(int[] extractedSeq, ref int[][] sequence)
+        {
+            for (int i = 0; i < extractedSeq.Length; i++)
+            {
+                sequence[i] = new int[i+1];
+                if (i == 0)
+                {
+                    sequence[0][0] = extractedSeq[0];
+                }
+                else CopyInsert(extractedSeq[i], ref sequence[i], sequence[i - 1]);
+            }
+        }
+        public static void CopyInsert(int inserted, ref int[] resultArray, int[] sourceArray)
+            {
+            int i = 0, k = 0;
+            while ((i + k) < resultArray.Length)
+            {
+                if (i == resultArray.Length - 1 && k == 0)
+                {
+                    k = 1;
+                    resultArray[i] = inserted;
+                }
+                else
+                if (sourceArray[i] > inserted && k==0 )
+                {
+                    resultArray[i] = inserted;
+                    k = 1;
+                }                
+                if ((i + k) < resultArray.Length) resultArray[i + k] = sourceArray[i];
+                i++;
+            }
+                     
+            }
+            }
+
+    }
+
