b837b90a1d6b12ce4facda40459cf87c94a6ec14
Florin <maris.flo88@gmail.com>
2016-05-15 15:23:55 +0300

    Algorythm sorting but not in a stable manner

---
+++ 7.2 ServiceCentre/7.2ServiceCentreTests/ServiceCentreTests.cs
@@ -0,0 +1,66 @@
+﻿using _7._2_ServiceCentre;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _7._2_ServiceCentre.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        Vehicle[] listToSort, sortedList;
+        [TestMethod()]
+        public void SetPriorityStabilityTest()
+        {
+            listToSort = new Vehicle[]
+            {new Vehicle() {owner="John",machineIdNumber= "B000JON",phoneNumber="0755732",priority=Priority.High },
+            new Vehicle() {owner="Ion",machineIdNumber= "CJ01ION",phoneNumber="075520000",priority=Priority.Low },
+            new Vehicle() {owner="Marcel",machineIdNumber= "CJ08MMM",phoneNumber="032000",priority=Priority.High },
+            new Vehicle() {owner="Florin",machineIdNumber= "MS88MVF",phoneNumber="075532",priority=Priority.Medium },
+            new Vehicle() {owner="Gigi",machineIdNumber= "GJ00GIG",phoneNumber="55732000",priority=Priority.Low },
+            new Vehicle() {owner="Radu",machineIdNumber= "MS00RAD",phoneNumber="07550000",priority=Priority.Medium },
+            new Vehicle() {owner="Catalin",machineIdNumber= "MM88CAT",phoneNumber="77770",priority=Priority.High },
+            new Vehicle() {owner="BOB",machineIdNumber= "B11BOB",phoneNumber="0900000000",priority=Priority.Low }
+            };
+
+            sortedList = new Vehicle[]
+            {new Vehicle() {owner="John",machineIdNumber= "B000JON",phoneNumber="0755732",priority=Priority.High },
+            new Vehicle() {owner="Marcel",machineIdNumber= "CJ08MMM",phoneNumber="032000",priority=Priority.High },
+            new Vehicle() {owner="Catalin",machineIdNumber= "MM88CAT",phoneNumber="77770",priority=Priority.High },
+            new Vehicle() {owner="Florin",machineIdNumber= "MS88MVF",phoneNumber="075532",priority=Priority.Medium },
+            new Vehicle() {owner="Radu",machineIdNumber= "MS00RAD",phoneNumber="07550000",priority=Priority.Medium },
+            new Vehicle() {owner="Ion",machineIdNumber= "CJ01ION",phoneNumber="075520000",priority=Priority.Low },
+            new Vehicle() {owner="Gigi",machineIdNumber= "GJ00GIG",phoneNumber="55732000",priority=Priority.Low },
+            new Vehicle() {owner="BOB",machineIdNumber= "B11BOB",phoneNumber="0900000000",priority=Priority.Low }
+            };
+            Service.SetPriority(ref listToSort);
+            CollectionAssert.AreEqual(listToSort, sortedList);
+        }
+        [TestMethod()]
+        public void SetPriorityTest()
+        {
+            listToSort = new Vehicle[]
+           {new Vehicle() {owner="John",machineIdNumber= "B000JON",phoneNumber="0755732",priority=Priority.High },
+            new Vehicle() {owner="Ion",machineIdNumber= "CJ01ION",phoneNumber="075520000",priority=Priority.Low },
+            new Vehicle() {owner="Marcel",machineIdNumber= "CJ08MMM",phoneNumber="032000",priority=Priority.High },
+            new Vehicle() {owner="Florin",machineIdNumber= "MS88MVF",phoneNumber="075532",priority=Priority.Medium },
+            new Vehicle() {owner="Gigi",machineIdNumber= "GJ00GIG",phoneNumber="55732000",priority=Priority.Low },
+            new Vehicle() {owner="Radu",machineIdNumber= "MS00RAD",phoneNumber="07550000",priority=Priority.Medium },
+            new Vehicle() {owner="Catalin",machineIdNumber= "MM88CAT",phoneNumber="77770",priority=Priority.High },
+            new Vehicle() {owner="BOB",machineIdNumber= "B11BOB",phoneNumber="0900000000",priority=Priority.Low }
+            };
+
+            sortedList = new Vehicle[]
+           {new Vehicle() {owner="John",machineIdNumber= "B000JON",phoneNumber="0755732",priority=Priority.High },
+            new Vehicle() {owner="Catalin",machineIdNumber= "MM88CAT",phoneNumber="77770",priority=Priority.High },
+            new Vehicle() {owner="Marcel",machineIdNumber= "CJ08MMM",phoneNumber="032000",priority=Priority.High },
+            new Vehicle() {owner="Florin",machineIdNumber= "MS88MVF",phoneNumber="075532",priority=Priority.Medium },
+            new Vehicle() {owner="Radu",machineIdNumber= "MS00RAD",phoneNumber="07550000",priority=Priority.Medium },
+            new Vehicle() {owner="Gigi",machineIdNumber= "GJ00GIG",phoneNumber="55732000",priority=Priority.Low },
+            new Vehicle() {owner="Ion",machineIdNumber= "CJ01ION",phoneNumber="075520000",priority=Priority.Low },
+            new Vehicle() {owner="BOB",machineIdNumber= "B11BOB",phoneNumber="0900000000",priority=Priority.Low }
+            };
+            Service.SetPriority(ref listToSort);
+            CollectionAssert.AreEqual(listToSort, sortedList);
+        }
+    }
+}
---
+++ 7.2 ServiceCentre/ServiceCentre/7.2ServiceCentre.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2_ServiceCentre
+{
+    public enum Priority
+    {
+        High,
+        Medium,
+        Low
+    }
+    public struct Vehicle
+    {
+        public string owner;
+        public string machineIdNumber;
+        public string phoneNumber;
+        public Priority priority;
+    }
+    public class Service
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void SetPriority(ref Vehicle[] list)
+        {
+            int wall1 = 0, wall2 = list.Length - 1;
+            int i = 0;
+            while (i <= wall2)
+            {
+                switch (list[i].priority)
+                {
+                    case (Priority)0:
+                        if (list[wall2].priority == (Priority)2) --wall2;
+                        else
+                        {
+                            if (i > wall1) Swap(ref list[wall1], ref list[i]);
+                            ++wall1; ++i;
+                        }
+                        break;
+                    case (Priority)1:
+                        ++i;
+                        break;
+                    case (Priority)2:
+                        Swap(ref list[wall2], ref list[i]);
+                        --wall2;
+                        break;
+                }
+            }
+        }
+        public static void Swap(ref Vehicle first, ref Vehicle second)
+        {
+            Vehicle aux = first;
+            first = second;
+            second = aux;
+        }
+    }
+}
