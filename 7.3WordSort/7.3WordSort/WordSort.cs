e363a1db41ae3376bb7ea69a51725a03da8b09c3
Florin <maris.flo88@gmail.com>
2016-05-19 23:16:42 +0300

    Not working yet, I am just using Git to clone my repository to a  new PC

---
+++ 7.3WordSort/7.3WordSort/WordSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._3WordSort
+{
+  public struct Word
+    {
+    public string word;
+    public int instances;
+    }
+  public  class WordSort
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void SortWords(string textSequence,ref Word[] list)
+        {
+            string[] words = textSequence.Split(' ', ',', '/', '.',':');
+            int i = 0;
+            while (words.Length>0)
+            {
+                Word currentWord = new Word() { word = words[0], instances = Count(words[0], ref words) };
+               
+
+            }  
+        }
+        public static int ReturnInsIndex();
+        public static int Count(string currentWord,ref string[] words )
+        {
+            int i = 0,k=0;
+            while (i<words.Length)
+            {
+                if (words[i] == currentWord)
+                {
+                    ShiftLeft(words, i);
+                    Array.Resize(ref words, words.Length - 1);
+                    k++;
+                }
+                else i++;
+            }
+            return k;
+        }
+        public static void ShiftLeft(string[] words,int start)
+        {
+            for (int i = start; i< words.Length - 1; i++)
+                words[i] = words[i + 1];
+        }
+    }
+}
---
+++ 7.3WordSort/WordSortTests/WordSortTests.cs
@@ -0,0 +1,30 @@
+﻿using _7._3WordSort;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _7._3WordSort.Tests
+{
+    //   Ordonează cuvintele
+    //   Găsește și ordonează cuvintele dintr-un text în funcție de numărul de apariții.
+
+    [TestClass()]
+    public class WordSortTests
+    {
+        [TestMethod()]
+        public void SortWordsTest()
+        {
+            string textSequence = "one one one three two two/five,four one, two one three, two, three.four:six";
+            Word[] outputList, expectedList;
+            outputList = new Word[1];
+            expectedList = new Word[]{ new Word() { word = "one", instances = 5 },
+                new Word() { word = "two", instances = 4 },
+                new Word() { word = "three", instances = 3 },
+                new Word() { word = "four", instances = 2 },
+                new Word() { word = "five", instances = 1 },
+                new Word() { word = "six", instances = 1 }
+                };
+            WordSort.SortWords(textSequence, ref outputList);
+            CollectionAssert.AreEqual(expectedList,outputList);
+        }
+    }
+}
