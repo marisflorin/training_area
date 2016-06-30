179f730b1fb0df673602146f8f2713cae1bf8cac
Florin <Florin@FLORIN-THINKPAD>
2016-07-01 01:05:30 +0300

    First and final tests ....

---
+++ 7.4 ElectionResults.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._4ElectionResults
+{
+    public struct Candidate
+    {
+        public string name;
+        public int numberOfVotes;
+        public Candidate(string name,int numberOfVotes)
+        {
+            this.name = name;
+            this.numberOfVotes = numberOfVotes;
+        }
+    }
+    public class ElectionResults
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void CentralizeVotes(string[] candidateNames, Candidate[,] resultList, ref Candidate[] list)
+        {
+            GenerateCandidates(candidateNames, ref list);
+            int numberOfCandidates = resultList.GetLength(0);
+            int numberOfLocations = resultList.GetLength(1);
+            for (int i=0;i<numberOfLocations-1;i++)
+                for (int j=0;j<numberOfLocations;j++)
+                {
+                    int index=GetIndex(resultList[i,j], list, 0, numberOfCandidates);
+                    list[index].numberOfVotes += resultList[i,j].numberOfVotes;
+
+                }
+            QuickSort(ref list, 0, list.Length - 1);
+
+        }
+       public static void GenerateCandidates(string[] candidateNames,ref Candidate[] list)
+        {
+            Array.Resize(ref list, candidateNames.Length);
+            for (int i = 0; i < candidateNames.Length; i++) list[i] = new Candidate(candidateNames[i], 0);                                  
+        }
+        public static int GetIndex(Candidate currentCandidate,Candidate[] list,int begining, int end)
+        {
+            int mid = (begining + end) / 2;
+            if (begining > end) return -1;
+            if (currentCandidate.name == list[mid].name) return mid;
+            if (String.Compare(currentCandidate.name,list[mid].name,true)>0) return GetIndex(currentCandidate, list, mid + 1, end);
+            else return GetIndex(currentCandidate, list, begining, mid-1);
+        }
+        public static void QuickSort(ref Candidate[] list, int left, int right)
+        {
+            if (left > -1 && right < list.Length && left < right)
+            {
+                int pivotIndex = 0;
+                if (left < right)
+                    pivotIndex = GetIndex(ref list, left, right);
+                QuickSort(ref list, left, pivotIndex - 1);
+                QuickSort(ref list, pivotIndex + 1, right);
+            }
+        }
+
+        public static int GetIndex(ref Candidate[] list, int left, int right)
+        {
+            int pivotIndex = left;
+            if (left > -1 && right < list.Length)
+                while (left != right)
+                {
+                    while (list[pivotIndex].numberOfVotes >= list[right].numberOfVotes && pivotIndex != right) right--;
+                    if (pivotIndex == right) break;
+                    else if (list[pivotIndex].numberOfVotes < list[right].numberOfVotes) Swap(ref list, right, ref pivotIndex);
+                    while (list[pivotIndex].numberOfVotes <= list[left].numberOfVotes && pivotIndex != left) left++;
+                    if (pivotIndex == left) break;
+                    else if (list[pivotIndex].numberOfVotes > list[left].numberOfVotes) Swap(ref list, left, ref pivotIndex);                }
+            return pivotIndex;
+        }
+        private static void Swap(ref Candidate[] list, int right, ref int pivotIndex)
+        {
+            Candidate aux = list[right];
+            list[right] = list[pivotIndex];
+            list[pivotIndex] = aux;
+            pivotIndex = right;
+        }
+    }
+}
---
+++ 7.4ElectionResults/ElectionResultsTests/ElectionTests.cs
@@ -0,0 +1,29 @@
+﻿using _7._4ElectionResults;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _7._4ElectionResults.Tests
+{
+    /*  Centralizare alegeri
+            La alegeri fiecare secție de votare trimite o listă de candidați și numărul de voturi.
+            Listele sunt ordonate descrescător în funcție de numărul de voturi.
+            Crează o funcție care să ajute la centralizarea voturilor.*/
+    [TestClass()]
+    public class ElectionTests
+    {
+        [TestMethod()]
+        public void CentralizeVotesTest()
+        {
+            string[] candidateNames = { "Andrei", "Cornel", "Daniel", "Ionel", "Sorin" };
+            Candidate[,] localResults = new Candidate[4, 5] { { new Candidate("Ionel", 90), new Candidate("Cornel", 60), new Candidate("Andrei", 30), new Candidate("Sorin", 21), new Candidate("Daniel", 1) },
+                                                    { new Candidate("Daniel", 80), new Candidate("Andrei", 50), new Candidate("Cornel", 20), new Candidate("Ionel", 10), new Candidate("Sorin", 2) },
+                                                    { new Candidate("Ionel", 84), new Candidate("Daniel", 73), new Candidate("Andrei", 50), new Candidate("Sorin", 4), new Candidate("Cornel", 1) },
+                                                     { new Candidate("Ionel", 104), new Candidate("Daniel", 60), new Candidate("Andrei", 20), new Candidate("Cornel", 10), new Candidate("Sorin", 2) },
+                                                    };
+            Candidate[] finalList = { new Candidate("Ionel", 288), new Candidate("Daniel", 214), new Candidate("Andrei", 150), new Candidate("Cornel", 91), new Candidate("Sorin", 29) };
+            Candidate[] centralisedList=new Candidate [0];
+            ElectionResults.CentralizeVotes(candidateNames,localResults, ref centralisedList);
+            CollectionAssert.AreEqual(finalList,centralisedList);
+        }
+    }
+}
