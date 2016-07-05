7db649f24f12fcaa8ae20a5e037e82f1d9676292
Florin <Florin@FLORIN-THINKPAD>
2016-07-05 11:19:19 +0300

    Alphabetical ordering using Bubble sort

---
+++ 7.5 Catalogue/7.5 Catalogue/Catalogue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._5_Catalogue
+{
+      public struct objectStudied
+    {
+        public List<int> grades;
+        decimal median;
+    }
+    public struct pupil
+    {
+        public string name;
+        public objectStudied Mathematics;
+        public objectStudied History;
+        public objectStudied Physics;
+        public objectStudied Geography;
+    }
+    public class Catalogue
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void AlphabeticOrder(ref pupil[] catalogue)
+        {
+            bool isOrdered = true;
+            do
+            {
+                isOrdered = true;
+                for (int i = 0; i < catalogue.Length-1; i++)
+                {                
+                    if (string.Compare(catalogue[i].name, catalogue[i + 1].name, true) > 0)
+                    {
+                        isOrdered = false;
+                        Swap(ref catalogue, i, i + 1);
+                    }
+                }
+            } while (!isOrdered); 
+
+        }
+        public static void Swap(ref pupil[] catalogue, int firstIndex,int secondIndex)
+        {
+            pupil temp = catalogue[firstIndex];
+            catalogue[firstIndex] = catalogue[secondIndex];
+            catalogue[secondIndex] = temp;
+        }
+        public static void GenerateCatalogue(string[] listOfPupils, ref pupil[] catalogue)
+        {
+            Array.Resize(ref catalogue, listOfPupils.Length);
+            for (int i = 0; i < listOfPupils.Length; i++)
+                catalogue[i] = new pupil { name = listOfPupils[i] };                
+        }
+    }
+}
---
+++ 7.5 Catalogue/CatalogueTests/CatalogueTests.cs
@@ -0,0 +1,35 @@
+﻿using _7._5_Catalogue;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _7._5_Catalogue.Tests
+{
+    /*
+      Catalog
+        Într-o clasă sunt 30 de elevi. Fiecare elev studiază 12 materii.
+        Pentru fiecare materie elevul primește una sau mai multe note.
+            Ordonează elevii alfabetic
+            Ordonează elevii în funcție de media generală
+            Găsește elevii cu o medie generală specifică
+            Găsește elevii cu cele mai multe note de 10
+            Găsește elevii cu cea mai mică medie generală
+            Implementează căutarea și ordonarea în mai multe metode.
+ */
+
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void AlphabeticOrderTest()
+        {
+            pupil[] catalogue = new pupil[0];
+            string[] listOfPupils = { "Petrescu George", "Ionescu George", "Albulescu Mihai", "Abrudan Silviu", "Marin Marin" };
+            Catalogue.GenerateCatalogue(listOfPupils, ref catalogue);
+            pupil[] sortedCatalogue = { new pupil { name = "Abrudan Silviu" },new pupil { name = "Albulescu Mihai" }, new pupil { name = "Ionescu George" }, new pupil { name = "Marin Marin" }, new pupil { name = "Petrescu George" } };
+            Catalogue.AlphabeticOrder(ref catalogue);
+            CollectionAssert.AreEqual(catalogue,sortedCatalogue);
+        }
+    }
+}
+
+
